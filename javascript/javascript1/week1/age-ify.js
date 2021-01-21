cc383bbb86d90f2f7e6f02d42ba39111a66fe33c
Saidemm <saymahtab@gmail.com>
2021-01-21 22:35:47 +0100

    Implemented age-ify

---
+++ javascript/javascript1/week1/age-ify.js
@@ -0,0 +1,7 @@
+// ******* Age-ify *******
+
+const yearOfBirth = 1979;
+const yearFuture = 2027;
+const age = yearFuture - yearOfBirth ;
+
+console.log ("You will be " + age + " in " + yearFuture + ".");
