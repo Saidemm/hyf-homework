3628eaf907658ae0e5c871cb594e661d8587cf60
Saidemm <saymahtab@gmail.com>
2021-01-21 22:37:42 +0100

    Implemented ez-namey

---
+++ javascript/javascript1/week1/ez-namey.js
@@ -0,0 +1,11 @@
+// ******* Ez Namey *******
+
+const firstWords = ["Easy", "Awsome", "Evolutionary", "Approachable", "Creative", "Authentic", "Trustworthy", "Motivated", "Professional", "Reliable"];
+const secondWords = ["Company", "Innovation", "Factory", "Corporation", "World", "House", "Future", "Future Planners", "Dream", "Business"];
+
+
+const randomFirstWordIndex = Math.floor(Math.random() * 10) + 0;
+const randomSecondWordIndex = Math.floor(Math.random() * 10) + 0;
+
+let newStartUpName = firstWords[randomFirstWordIndex] + " " + secondWords[randomSecondWordIndex];
+console.log("The startup: \"" + newStartUpName + "\" contains " + newStartUpName.length + " characters.");
