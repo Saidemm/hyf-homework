d8d8db1c5a6d08494cf8c554a24b2ce5c79b0051
Saidemm <saymahtab@gmail.com>
2021-02-19 15:52:35 +0100

    Added homework javascript2-week1

---
+++ javascript/javascript2/week1/homework/findShortestWord/index.js
@@ -0,0 +1,45 @@
+//Find the shortest word 
+const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
+function getShorterWord(danishWords){ 
+    const lengthOfDanishWords = [];
+    danishWords.forEach(word => {
+    lengthOfDanishWords.push(word.length)
+});
+    let shorterDanishWord = Math.min.apply(Math, lengthOfDanishWords);
+    danishWords.forEach(word => {
+       if (word.length == shorterDanishWord){
+        console.log(`Shortest Danish word : ${word}`);
+       }
+    });
+
+};
+getShorterWord(danishWords); 
+
+
+//This program finds and counts the Danish letters
+
+const danishLetters = ["å", "æ", "ø"];
+function numbersOfDanishLetters(danishString) {
+    const numberOfå = (danishString.match(/å/g) || []).length;
+    const numberOfæ = (danishString.match(/æ/g) || []).length;
+    const numberOfø = (danishString.match(/ø/g) || []).length;
+    const totalnumOfdanishLetters = numberOfå + numberOfæ + numberOfø;
+
+    let info = {};
+    info.total = totalnumOfdanishLetters;
+    if(numberOfå!=0){
+        info["å"] = numberOfå;
+    }
+    if(numberOfæ!=0){
+        info["æ"] = numberOfæ;
+    }
+    if(numberOfø!=0){
+        info["ø"] = numberOfø;
+    }
+    return info;
+};
+const danishString = "Jeg har en blå bil";
+console.log(numbersOfDanishLetters(danishString)); // returns {total: 1, å: 1}
+
+const danishString2 = "Blå grød med røde bær";
+console.log(numbersOfDanishLetters(danishString2)); // returns {total: 4, æ: 1, ø: 2, å: 1}
---
+++ javascript/javascript2/week1/homework/hyf-bay/main.js
@@ -0,0 +1,26 @@
+console.log("Script loaded");
+
+const products = getAvailableProducts();
+console.log(products);
+
+function renderProducts(products) {
+    // your code here
+    const productList = document.querySelector("ul");
+    products.forEach(product => {
+        let li = document.createElement("li");
+        let title = document.createElement("h2");
+        let productPrice = document.createElement("p");
+        let productRating = document.createElement("p");
+
+        title.innerHTML = product.name;
+        li.appendChild(title);
+        productPrice.innerHTML = "price: " + product.price;
+        li.appendChild(productPrice);
+        productRating.innerHTML = "rating: " + product.rating;
+        li.appendChild(productRating);
+
+        productList.appendChild(li);
+    });
+}
+
+renderProducts(products); // This should create the ul and the li's with the individual products details
