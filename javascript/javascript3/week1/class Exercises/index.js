0d4091443926e9d13ce93113f88bc92a53c7b646
Saidemm <saymahtab@gmail.com>
2021-03-06 14:50:01 +0100

    exerciseJS3/1

---
+++ javascript/javascript3/week1/class Exercises/index.js
@@ -0,0 +1,16 @@
+const order1 = {
+    "orderName": "Burger Menu",
+    "orderId": "39",
+    "price": "400",
+    "listOfDrinks": [
+      "Classic Beer",
+      "Cola",
+      "Water"
+    ],
+    "orderExtras": {
+        "cheese":"Camember",
+        "Fries":"Sweet potato",
+        "dessert":"Creme brule"
+    }
+  }
+  
