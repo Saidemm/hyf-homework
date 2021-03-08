4f8356f4da1996ef96949c24b839fd84eb5595c3
Saidemm <saymahtab@gmail.com>
2021-03-08 13:03:25 +0100

    class-exercise/Js3/week1

--- javascript/javascript3/week1/class Exercises/index.js
+++ javascript/javascript3/week1/class Exercises/index.js
@@ -1,4 +1,5 @@
-const order1 = {
+const order1 = [
+  {
     "orderName": "Burger Menu",
     "orderId": "39",
     "price": "400",
@@ -8,9 +9,64 @@ const order1 = {
       "Water"
     ],
     "orderExtras": {
-        "cheese":"Camember",
-        "Fries":"Sweet potato",
-        "dessert":"Creme brule"
+      "cheese": "Camember",
+      "Fries": "Sweet potato",
+      "dessert": "Creme brule"
     }
   }
-  
+]
+
+
+
+// Fetch
+fetch('https://yesno.wtf/api/')
+  .then(response => response.json())
+  .then(yesOrNoData => {
+      console.log(yesOrNoData);
+      const span = document.createElement("span");
+      span.innerHtml = yesOrNoData.answer;
+      document.body.appendChild(span);
+      // HERE IS WHERE YOU WRITE YOUR CODE!!!!!!!!
+  });
+
+
+
+
+setInterval(function(){
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => response.json())
+  .then(dogFanWebsite => {
+      console.log(dogFanWebsite);
+    const image = document.createElement("img");
+    image.src = dogFanWebsite.message;
+    document.body.appendChild(image)
+  })
+}, 2000)
+
+fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(breeds => {
+    console.log(breeds);
+
+   
+ 
+
+const dogBreedsArrey = Object.keys(breeds.message);
+console.log(dogBreedsArrey);
+const randomBreed = dogBreedsArrey[Math.floor(Math.random() * dogBreedsArrey.length)];
+console.log(randomBreed);
+
+
+fetch(`https://dog.ceo/api/breed/${randomBreed}/images/random`)
+  .then(response => response.json())
+  .then(randomImage => {
+    console.log(randomImage);
+    const imageBreed = document.createElement("img");
+    document.body.appendChild(imageBreed);
+    imageBreed.src = randomImage.message;
+
+  });
+
+})
+
+
