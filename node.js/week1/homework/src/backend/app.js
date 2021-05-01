fecbacbac5eabcb11fa82a28c5ae938870bcd091
Saidemm <saymahtab@gmail.com>
2021-05-01 14:48:20 +0200

    Implemented NodeJs/week1/HW

--- node.js/week1/homework/src/backend/app.js
+++ node.js/week1/homework/src/backend/app.js
@@ -3,10 +3,60 @@ const app = express();
 
 // import data here
 const meals = require("./data/meals");
+const reviews = require("./data/reviews");
+const reservations = require("./data/reservations");
+
+function mealsAndReviews() {
+  const mealsWithReviews = meals.map((meal) => {
+    meal.reviews = reviews.filter((review) =>
+      review.mealId === meal.id
+    );
+    return meal;
+  });
+  return mealsWithReviews;
+}
+
+const allMealsWithReviews = mealsAndReviews();
+
+function cheapMealsGroup() {
+  return cheapMeals = allMealsWithReviews.filter((meal) => meal.price < 80);
+}
+
+function largeMealsGroup() {
+  return largeMeals = allMealsWithReviews.filter((meal) => meal.maxNumberOfGuests >= 6);
+}
+
 
 // this is where you will be adding your routes
 app.get("/", async (request, response) => {
-  response.send("asd");
+  response.send("Meal Sharing Web App");
+});
+
+app.get("/meals", async (request, response) => {
+  response.json(allMealsWithReviews);
+});
+
+app.get("/cheap-meals", async (request, response) => {
+  response.json(cheapMealsGroup());
+});
+
+app.get("/large-meals", async (request, response) => {
+  response.json(largeMealsGroup());
+});
+
+// random meal (including it's reviews)
+app.get("/meal", async (request, response) => {
+  const random = Math.floor(Math.random() * allMealsWithReviews.length);
+  response.send(meals[random]);
+});
+
+app.get("/reservations", async (request, response) => {
+  response.json(reservations);
+});
+
+app.get("/reservation", async (request, response) =>{
+  const random = Math.floor(Math.random() * reservations.length);
+  response.send(reservations[random]);
 });
 
 module.exports = app;
--- node.js/week1/homework/warmup.js
+++ node.js/week1/homework/warmup.js
@@ -1,1 +1,26 @@
-console.log("inside warmup file");
+class Circle {
+
+    constructor(radius) {
+        this.radius = radius;
+    }
+    getDiameter() {
+        return this.radius * 2;
+    }
+    getCircumference() {
+        return this.radius * 2 * Math.PI;
+    }
+    getArea() {
+        return this.radius * this.radius * Math.PI; 
+    }
+    
+}
+
+const circleOne = new Circle(20);
+console.log(circleOne.getDiameter());
+console.log(circleOne.getCircumference());
+console.log(circleOne.getArea());
+
+const circleTwo = new Circle(2);
+console.log(circleTwo.getDiameter());
+console.log(circleTwo.getCircumference());
+console.log(circleTwo.getArea());
