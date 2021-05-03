8c1fa3cdb1de29c78f58f92b885d44e99011d80a
Saidemm <saymahtab@gmail.com>
2021-05-03 16:26:54 +0200

    class exercise w2/node2

--- node.js/week2/exercise-template/src/backend/api/movies-router.js
+++ node.js/week2/exercise-template/src/backend/api/movies-router.js
@@ -5,7 +5,13 @@ const movies = require("../data/movies.json");
 
 router.get("/", async (request, response) => {
   console.log(movies);
-  response.send({ data: [] });
+  response.send({ data: [movies] });
 });
+
+router.get("/:year", async (request, response) => {
+  response.send({ data: movies.filter(movie => movie.year == Number(request.params.year)) })
+});
+
+
 
 module.exports = router;
--- node.js/week2/exercise-template/src/backend/app.js
+++ node.js/week2/exercise-template/src/backend/app.js
@@ -7,12 +7,19 @@ const moviesRouter = require("./api/movies-router");
 // This is where you want to create your is chrome browser middleware (second exercise)
 
 
+app.use((req,res,next)=>{
+    request.isChromeBrowser = request.headers["user-agent"].includes('chrome')
+    console.log(request.isChromeBrowser);
+
+    next()
+})
 // app.use binds middleware to your application. You can give app.use a path and router. The mini router will take care of all requests with the path
 app.use("/api/movies", moviesRouter);
 
 
 // For testing purposes only!! Used to check if the middleware function is implemented correctly
 app.get("/middleware-test", async (request, response) => {
+    console.log(request.headers);
     response.send({ data: 
         { "isChromeBrowser": request.isChromeBrowser }
     });
