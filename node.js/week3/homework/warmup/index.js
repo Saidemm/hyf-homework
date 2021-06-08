ce92f625e0f68f0887e30b767402ebcba3fffc38
Saidemm <saymahtab@gmail.com>
2021-06-08 23:28:24 +0200

    implemented some changes and feedback

--- node.js/week3/homework/warmup/index.js
+++ node.js/week3/homework/warmup/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 
-
 app.use(
     express.urlencoded({
         extended: true
@@ -10,114 +9,113 @@ app.use(
 app.use(express.json());
 
 
+
+function sendResponse(result, response) {
+    isNaN(result) ? response.status(400).send("Input must be number") : response.send(`${result}`);
+}
+
+
 app.get("/", (req, res) => res.send("nodejs week3 homework"));
 
 app.get("/calculator/add", (request, response) => {
-    console.log(request.query);
+
     try {
         const paramValueArray = Object.values(request.query)
-            .flatMap(x => x)
-            .map(x => parseInt(x));
+            .flatMap(x => parseInt(x));
         const sum = paramValueArray.reduce((requestParam, sum) => requestParam + sum, 0);
-        console.log(sum);
 
-        if (isNaN(sum)) {
-            response.status(400).json({ error: "Input must be number" });
-            return
-        } else {
-            response.send(`${sum}`);
-        }
+        sendResponse(sum, response);
+
     } catch (error) {
-        console.log(error)
+
         response.status(500).send({ error: "Internal Server Error." });
     }
 })
+
+
 
 app.get("/calculator/substract", (request, response) => {
     console.log(request.query);
     try {
         const paramValueArray = Object.values(request.query)
-            .flatMap(x => x)
-            .map(x => parseInt(x));
+            .flatMap(x => parseInt(x));
         const substract = paramValueArray.slice(1).reduce((requestParam, substract) => requestParam - substract, paramValueArray[0]);
-        console.log(substract);
 
-        if (isNaN(substract)) {
-            response.status(400).json({ error: "Input must be number" });
-            return
-        } else {
-            response.send(`${substract}`);
-        }
+        sendResponse(substract, response);
+       
     } catch (error) {
-        console.log(error)
+
         response.status(500).send({ error: "Internal Server Error." });
     }
 })
 
 app.get("/calculator/multiply", (request, response) => {
-    console.log(request.query);
+
     try {
         const paramValueArray = Object.values(request.query)
-            .flatMap(x => x)
-            .map(x => parseInt(x));
+            .flatMap(x => parseInt(x));
         const multiple = paramValueArray.reduce((requestParam, multiple) => requestParam * multiple, 1);
-        console.log(multiple);
 
-        if (isNaN(multiple)) {
-            response.status(400).json({ error: "Input must be number" });
-            return
-        } else {
-            response.send(`${multiple}`);
-        }
+        sendResponse(multiple, response);
     } catch (error) {
-        console.log(error)
+
         response.status(500).send({ error: "Internal Server Error." });
     }
 })
 
 app.get("/calculator/division", (request, response) => {
-    console.log(request.query);
+
     try {
         const paramValueArray = Object.values(request.query)
-            .flatMap(x => x)
-            .map(x => parseInt(x));
-        const division = paramValueArray.slice(1).reduce((requestParam, division) => requestParam / division, paramValueArray[0]);
-        console.log(division);
+            .flatMap(x => parseInt(x));
 
-        if (isNaN(division)) {
-            response.status(400).json({ error: "Input must be number" });
-            return
-        } else {
-            response.send(`${division}`);
-        }
+        const division = paramValueArray.slice(1).reduce((requestParam, division) => requestParam / division, paramValueArray[0]);
+
+        sendResponse(division, response);
+        
     } catch (error) {
-        console.log(error)
+
         response.status(500).send({ error: "Internal Server Error." });
     }
 })
 
 
-app.post("/calculator/multiply", (request, response) => {
+//Getting data through the request body using POST
+
+app.post("/calculator/:operator", (request, response) => {
 
     try {
+        const operator = request.params.operator;
         const paramValueArray = Object.values(request.body)
-            .flatMap(x => x)
-            .map(x => parseInt(x));
-             console.log(paramValueArray);
-        const multiple = paramValueArray.reduce((requestParam, multiple) => requestParam * multiple, 1);
-        console.log(multiple);
+            .flatMap(x => parseInt(x));
 
-        if (isNaN(multiple)) {
-            response.status(400).json({ error: "Input must be number" });
-            return
-        } else {
-            response.send(`This:${multiple}`);
+        if(operator == "add") {
+            const sum = paramValueArray.reduce((requestParam, sum) => requestParam + sum, 0);
+            sendResponse(sum, response);
+
+        } else if(operator == "substract") {
+            const substract = paramValueArray.slice(1).reduce((requestParam, substract) => requestParam - substract, paramValueArray[0]);
+            sendResponse(substract, response);
+
+        } else if(operator == "multiply") {
+            const multiple = paramValueArray.reduce((requestParam, multiple) => requestParam * multiple, 1);
+            sendResponse(multiple, response);
+
+        } else if(operator == "division") {
+            const division = paramValueArray.slice(1).reduce((requestParam, division) => requestParam / division, paramValueArray[0]);
+            sendResponse(division, response);
+        }  else {
+            res.status(404).json({
+                error: "there is no such operator with that name"
+            });
         }
+
+        
     } catch (error) {
         console.log(error)
         response.status(500).send({ error: "Internal Server Error." });
     }
-})
+});
 
 
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
